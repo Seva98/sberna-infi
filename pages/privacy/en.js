cc4d4cb4e8cd41c1bb4a692519406cfaecdf6efa
Ondrej Sevcik <Ondrej Sevcik>
2021-11-16 20:40:33 +0100

    fixed single quote issue

--- pages/privacy/en.js
+++ pages/privacy/en.js
@@ -3,7 +3,7 @@ import Layout from '../../components/common/layout';
 const PrivacyEN = () => {
   return (
     <Layout>
-      <div className="text-center">Mobile application Sběrna Krmelín doesn't collect any personal data.</div>
+      <div className="text-center">{"Mobile application Sběrna Krmelín doesn't collect any personal data."}</div>
     </Layout>
   );
 };
