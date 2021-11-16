f52f55bec2257567ab1ff2194f7150e5239427b3
Ondrej Sevcik <Ondrej Sevcik>
2021-11-16 20:37:11 +0100

    added privacy pages

---
+++ pages/privacy/cz.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/common/layout';
+
+const PrivacyCZ = () => {
+  return (
+    <Layout>
+      <div className="text-center">Mobilní aplikace Sběrna Krmelín nesbírá žádné osobní údaje.</div>
+    </Layout>
+  );
+};
+
+export default PrivacyCZ;
---
+++ pages/privacy/en.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/common/layout';
+
+const PrivacyEN = () => {
+  return (
+    <Layout>
+      <div className="text-center">Mobile application Sběrna Krmelín doesn't collect any personal data.</div>
+    </Layout>
+  );
+};
+
+export default PrivacyEN;
